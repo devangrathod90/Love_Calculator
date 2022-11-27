44a2a85f97a54781dc840d647444150c50c6a4fc
Devang Rathod <96238249+devangrathod90@users.noreply.github.com>
2022-11-27 23:59:15 +0530

    Add files via upload

---
+++ page2/result.js
@@ -0,0 +1,12 @@
+function loveCalculator(){
+    
+    percentage = Math.random()*100;
+
+    percentage = Math.floor(percentage) + 1;
+    var result = percentage;
+    alert(result);
+}
+
+if(result <= 30){
+    document.
+}
